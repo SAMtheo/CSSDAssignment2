34de9e6dd4235febe751344eb4ddba1ff2d834a2
Robin Davies <33762083+Echo319@users.noreply.github.com>
2019-03-04 14:04:57 +0000

    reworked getters and setters for users

--- EVotingSystem/EVotingSystem/User.cs
+++ EVotingSystem/EVotingSystem/User.cs
@@ -8,9 +8,9 @@ namespace EVotingSystem
 {
     public abstract class User
     {
-        private string username { get; }
-        private string hashedPassword { get; }
-        private int authAttempts { get; set; }
+        private string username;
+        private string hashedPassword;
+        private int authAttempts;
 
         public User(string username, string password)
         {
@@ -18,14 +18,35 @@ namespace EVotingSystem
             this.hashedPassword = password;
             this.authAttempts = 0;
         }
+
+        public string getUsername()
+        {
+            return username;
+        }
+
+        public string getHashedPassword()
+        {
+            return hashedPassword;
+        }
+
+        public int getAuthAttempts()
+        {
+            return authAttempts;
+        }
+
+        public void resetAuthAttempts()
+        {
+            authAttempts = 0;
+        }
+
     }
 
     public class Voter : User
     {
-        private bool isEligible { get; set; }
-        private string firstName { get; }
-        private string lastName { get; }
-        private DateTime dob { get;  }
+        private bool isEligible;
+        private string firstName;
+        private string lastName;
+        private DateTime dob;
         // TODO: Address
         // TODO: List<Elections>
             
@@ -38,6 +59,29 @@ namespace EVotingSystem
             this.dob = dob;
         }
     
-    
+        public bool getIsEligible()
+        {
+            return isEligible;
+        }
+
+        public void setIsEligible(bool isEligible)
+        {
+            this.isEligible = isEligible;
+        }
+
+        public string getFirstname()
+        {
+            return firstName;
+        }
+
+        public string getLastname()
+        {
+            return lastName;
+        }
+
+        public DateTime getDob()
+        {
+            return dob;
+        }
     }
 }
