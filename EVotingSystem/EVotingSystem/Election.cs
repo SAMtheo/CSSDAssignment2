d2144d8891cf80f04f4be7faec7647aec03d2da1
b5016752 <b5016752@my.shu.ac.uk>
2019-03-04 11:16:18 +0000

    Created initial Election class
    This contains the Election and Candidate classes.

---
+++ EVotingSystem/EVotingSystem/Election.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace EVotingSystem
+{
+    class Election
+    {
+        String electionName;
+        List<Candidate> candidates;
+        DateTime startDate;
+        DateTime endDate;
+
+        public Boolean voteChoice(Candidate chosenVote) {
+            // [TODO] Implement user voting for a candidate
+            if (validateVoteTime())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean validateVoteTime() {
+            DateTime now = DateTime.Now;
+            if ((now.CompareTo(startDate) >= 0) && (now.CompareTo(endDate) <= 0)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    class Candidate
+    {
+        private String name;
+        private String party;
+        private Bitmap photo;
+
+        public Candidate(String name, String party, String photoFilePath)
+        {
+            this.name = name;
+            this.party = party;
+
+            if (System.IO.File.Exists(photoFilePath))
+            {
+                photo = new Bitmap(photoFilePath);
+            }
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getParty()
+        {
+            return party;
+        }
+
+        public Bitmap getPicture()
+        {
+            return photo;
+        }
+    }
+}
