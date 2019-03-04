2d3b104bf4124861e45af69326e861cb380143e2
b5016752 <b5016752@my.shu.ac.uk>
2019-03-04 12:13:44 +0000

    Added comments

--- EVotingSystem/EVotingSystem/Election.cs
+++ EVotingSystem/EVotingSystem/Election.cs
@@ -14,18 +14,32 @@ namespace EVotingSystem
         DateTime startDate;
         DateTime endDate;
 
+        /*
+         * voteChance - Chooses a vote to chosen candidate
+         * 
+         * Params:
+         * Candidate chosenVote - The users chosen candidate
+         * 
+         * returns bool - vote registered
+         */
         public bool vote(Candidate chosenVote) {
             // [TODO] Implement user voting for a candidate
-            if (validateVoteTime())
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+                if (validateVoteTime())
+                {
+                    //Storage.vote(candidate.name)
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
         }
 
+        /*
+         * validateVoteTime - Validates that the current time is still a valid voting time for this election
+         * 
+         * returns bool - if still valid voting time
+         */
         public bool validateVoteTime() {
             DateTime now = DateTime.Now;
             if ((now.CompareTo(startDate) >= 0) && (now.CompareTo(endDate) <= 0)) {
@@ -40,6 +54,7 @@ namespace EVotingSystem
     {
         private string name;
         private string party;
+        private Bitmap photo; // Photo of the candidate used on UI
 
         public Candidate(string name, string party, string photoFilePath)
         {
