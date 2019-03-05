fbb25287dceb9bfdfcfd5fa4fdfbae941f104d35
M1K8 <mikepress88@gmail.com>
2019-03-05 14:36:01 +0000

    Initial commit

--- EVotingSystem/EVotingSystem/AdminGUI.cs
+++ EVotingSystem/EVotingSystem/AdminGUI.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace EVotingSystem
 {
@@ -14,6 +15,48 @@ namespace EVotingSystem
     {
         public AdminGUI()
         {
+            string elecFile = "election.votes";
+            Dictionary<string, int> votes = new Dictionary<string, int>();
+
+            //check if file exists, if it doesnt throw a hissy fit
+            try
+            {
+                FileStream F = new FileStream(elecFile, FileMode.Open, FileAccess.Read);
+                F.Close();
+            } catch (Exception e)
+            {
+                throw e;
+            }
+
+
+            using (StreamReader sr = new StreamReader(elecFile))
+            {
+                Task<string> fileContents = sr.ReadToEndAsync();
+                string f = fileContents.Result;
+                //create a list of candidate:votes pairs
+                string[] elections = f.Split(',');
+
+                string[] elecPair = new string[2];
+                //read in all existing values
+                foreach (string s in elections)
+                {
+                    //should always return a pair, but .Split returns a []
+                    elecPair = s.Split(':');
+
+                    if (elecPair.Length != 2)
+                    {
+                        throw new Exception();
+                    }
+                    //covert string val to number
+                    int votesNum = Convert.ToInt32(elecPair[1]);
+
+                    //and add them to our dictionary
+                    votes.Add(elecPair[0], votesNum);
+
+                }
+            }
+
+
             InitializeComponent();
         }
     }
