67fab750c4c0e54af6c1679f01a553128ae81983
M1K3L08 <mikepress88@gmail.com>
2019-03-19 17:20:37 +0000

    Check for valid text input - dont proceed if not present

--- EVotingSystem/EVotingSystem/UserSignup.Designer.cs
+++ EVotingSystem/EVotingSystem/UserSignup.Designer.cs
@@ -40,6 +40,16 @@
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
@@ -73,7 +83,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(209, 45);
+            this.textBox5.Location = new System.Drawing.Point(225, 45);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(146, 20);
             this.textBox5.TabIndex = 4;
@@ -89,14 +99,14 @@
             // 
             this.textBox7.Location = new System.Drawing.Point(225, 242);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(146, 20);
+            this.textBox7.Size = new System.Drawing.Size(67, 20);
             this.textBox7.TabIndex = 6;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(225, 304);
+            this.textBox8.Location = new System.Drawing.Point(12, 325);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(146, 20);
+            this.textBox8.Size = new System.Drawing.Size(193, 20);
             this.textBox8.TabIndex = 7;
             // 
             // checkBox1
@@ -104,26 +114,27 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(12, 504);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.Size = new System.Drawing.Size(136, 17);
             this.checkBox1.TabIndex = 8;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "Send Password In Post";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(12, 436);
+            this.textBox9.Location = new System.Drawing.Point(12, 398);
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(193, 20);
             this.textBox9.TabIndex = 9;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(354, 579);
+            this.button1.Location = new System.Drawing.Point(261, 579);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(109, 68);
             this.button1.TabIndex = 10;
-            this.button1.Text = "button1";
+            this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
@@ -131,22 +142,126 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(109, 68);
             this.button2.TabIndex = 11;
-            this.button2.Text = "button2";
+            this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "First Name";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Surname";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "House Name / Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Street Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "County / State";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(222, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "City / Town";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(222, 226);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "ZIP Code";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(9, 309);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(106, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "NI / GOV ID Number";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(9, 382);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Email Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 443);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Confirm Email";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(12, 304);
+            this.textBox10.Location = new System.Drawing.Point(12, 459);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(80, 20);
-            this.textBox10.TabIndex = 12;
+            this.textBox10.Size = new System.Drawing.Size(193, 20);
+            this.textBox10.TabIndex = 23;
             // 
             // UserSignup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(475, 661);
+            this.ClientSize = new System.Drawing.Size(382, 651);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox9);
@@ -181,6 +296,16 @@
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox10;
     }
 }
--- EVotingSystem/EVotingSystem/UserSignup.cs
+++ EVotingSystem/EVotingSystem/UserSignup.cs
@@ -15,6 +15,7 @@ namespace EVotingSystem
 {
     public partial class UserSignup : Form
     {
+        private bool proceed = true;
         public UserSignup()
         {
             InitializeComponent();
@@ -22,30 +23,142 @@ namespace EVotingSystem
 
         private void UserSignup_Load(object sender, EventArgs e)
         {
-            //to login to this temp email account, go to https://mail.ionos.co.uk/ and use the below credentials. Please use carefully :)
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("Admin", "temp@m1k.me"));
-            message.To.Add(new MailboxAddress("User", "temp@m1k.me"));
-            message.Subject = "Voter Registration Credentials for ";// + user ;
 
-            message.Body = new TextPart("plain")
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            proceed = true;
+            string user = "";
+            string email = "";
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
             {
-                Text = ""
-            };
-
-            using (var client = new SmtpClient())
-            {
-                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
-                client.ServerCertificateValidationCallback = (s, c, h, e_) => true;
-
-                client.Connect("smtp.ionos.co.uk", 587, false);
-
-                // Note: only needed if the SMTP server requires authentication
-                client.Authenticate("temp@m1k.me", "password");
-
-                client.Send(message);
-                client.Disconnect(true);
+                proceed = false;
+                label1.ForeColor = Color.Red;
             }
+            else label1.ForeColor = Color.Black;
+
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                proceed = false;
+                label3.ForeColor = Color.Red;
+            }
+            else label3.ForeColor = Color.Black;
+
+            if (string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                proceed = false;
+                label4.ForeColor = Color.Red;
+            }
+            else label4.ForeColor = Color.Black;
+
+            if (string.IsNullOrWhiteSpace(textBox4.Text))
+            {
+                proceed = false;
+                label5.ForeColor = Color.Red;
+            }
+            else label5.ForeColor = Color.Black;
+
+            if (string.IsNullOrWhiteSpace(textBox5.Text))
+            {
+                proceed = false;
+                label2.ForeColor = Color.Red;
+            }
+            else label2.ForeColor = Color.Black;
+
+            if (string.IsNullOrWhiteSpace(textBox6.Text))
+            {
+                proceed = false;
+                label7.ForeColor = Color.Red;
+            }
+            else label7.ForeColor = Color.Black;
+
+            if (string.IsNullOrWhiteSpace(textBox7.Text))
+            {
+                proceed = false;
+                label8.ForeColor = Color.Red;
+            }
+            else label8.ForeColor = Color.Black;
+
+            if (string.IsNullOrWhiteSpace(textBox8.Text))
+            {
+                proceed = false;
+                label9.ForeColor = Color.Red;
+            }
+            else label9.ForeColor = Color.Black;
+
+            if (string.IsNullOrWhiteSpace(textBox9.Text))
+            {
+                proceed = false;
+                label10.ForeColor = Color.Red;
+            }
+            else label10.ForeColor = Color.Black;
+
+            if (string.IsNullOrWhiteSpace(textBox10.Text))
+            {
+                proceed = false;
+                label6.ForeColor = Color.Red;
+            }
+            else label6.ForeColor = Color.Black;
+
+
+            sendMsg(proceed, user, email);
+        }
+
+
+        public void sendMsg(bool p, string st, string em)
+        {
+            if (p)
+            {
+                //to login to this temp email account, go to https://mail.ionos.co.uk/ and use the below credentials. Please use carefully :)
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Admin", "temp@m1k.me"));
+                message.To.Add(new MailboxAddress(st, "temp@m1k.me"));
+                message.Subject = "Voter Registration Credentials for " + st ;
+
+                string randomPass = "password" + st;
+
+                message.Body = new TextPart("plain")
+                {
+                    Text = "Password is " + randomPass
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                    client.ServerCertificateValidationCallback = (s, c, h, e_) => true;
+
+                    client.Connect("smtp.ionos.co.uk", 587, false);
+
+                    client.Authenticate("temp@m1k.me", "password");
+
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+            } else
+            {
+                 MessageBox.Show("Please make sure all boxes are filled","ERROR - Incomplete Form",MessageBoxButtons.OK);
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
