3ffeb0963922486521622be5ebd47098307448b3
M1K3L08 <mikepress88@gmail.com>
2019-03-19 00:59:06 +0000

    Successfully sends blank email to desired address

--- EVotingSystem/EVotingSystem/UserSignup.cs
+++ EVotingSystem/EVotingSystem/UserSignup.cs
@@ -22,7 +22,30 @@ namespace EVotingSystem
 
         private void UserSignup_Load(object sender, EventArgs e)
         {
+            //to login to this temp email account, go to https://mail.ionos.co.uk/ and use the below credentials. Please use carefully :)
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Admin", "temp@m1k.me"));
+            message.To.Add(new MailboxAddress("User", "temp@m1k.me"));
+            message.Subject = "Voter Registration Credentials for ";// + user ;
 
+            message.Body = new TextPart("plain")
+            {
+                Text = ""
+            };
+
+            using (var client = new SmtpClient())
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client.ServerCertificateValidationCallback = (s, c, h, e_) => true;
+
+                client.Connect("smtp.ionos.co.uk", 587, false);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("temp@m1k.me", "password");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
     }
 }
