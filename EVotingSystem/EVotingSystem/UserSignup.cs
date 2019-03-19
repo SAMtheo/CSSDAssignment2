10373ac62ca9082b9190eb7e5394e8b339cdb29a
M1K3L08 <mikepress88@gmail.com>
2019-03-19 00:50:36 +0000

    Inital commit for user registration

---
+++ EVotingSystem/EVotingSystem/UserSignup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+namespace EVotingSystem
+{
+    public partial class UserSignup : Form
+    {
+        public UserSignup()
+        {
+            InitializeComponent();
+        }
+
+        private void UserSignup_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
