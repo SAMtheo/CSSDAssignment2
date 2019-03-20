4d3ff56d9e81956520ba2f373f8902da9667468e
Joe Shapiro <b5011296@my.shu.ac.uk>
2019-03-20 17:32:46 +0000

    Encryption/Decryption Test

---
+++ EVotingSystem/EVotingSystemTests/SecureStorageTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EVotingSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVotingSystem.Tests
+{
+    [TestClass()]
+    public class SecureStorageTests
+    {
+        /// <summary>
+        /// Test the encrypt and decrypt algorithm by working with a single string.
+        /// </summary>
+        [TestMethod()]
+        public void EncryptDecryptTest()
+        {
+            // Data to use for the test
+            String plainTextTest = "Testing string, this string should be encrypted and decrypted back.";
+            String passcode      = "TESTINGPASSWORD9991110009991100";
+
+            String encryptedString;
+            String decryptedString;
+
+            // Encrypt the string with the passcode
+            encryptedString = SecureStorage.Encrypt(plainTextTest, passcode);
+            Console.WriteLine(encryptedString);
+            decryptedString = SecureStorage.Decrypt(encryptedString, passcode);
+
+
+            if (!plainTextTest.Equals(decryptedString)) {
+                Assert.Fail();
+            }
+        }
+    }
+}
